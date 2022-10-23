b7a4930cbc3cd8c5df11c30ce8f71a59e631a09a
andreadldev <andrea.delisi2@gmail.com>
2022-10-23 17:24:42 +0200

    Aggiungi script per mostrare dati biglietto

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const btn = document.getElementById("btnTicket");
 const age = document.getElementById("age");
 const km = document.getElementById("km");
+const name = document.getElementById("name");
 
 btn.addEventListener("click", function() {
     if (km.value > 500 || km.value < 1) {
@@ -12,6 +13,11 @@ btn.addEventListener("click", function() {
                 const price = km.value * 0.21;
                 console.log("cliente maggiorenne");
                 console.log(`Il prezzo è ${price.toFixed(2)}€`);
+                document.getElementById("toggle").classList.remove("d-none");
+                document.querySelector("#toggle span>strong").innerHTML = `${name.value}`;
+                document.getElementById("train").innerHTML = Math.floor(Math.random() * 10) + 1;
+                document.getElementById("CPcode").innerHTML = Math.floor(Math.random() * (95000 - 90000)) + 95000;
+                document.getElementById("ticketPrice").innerHTML = `${price.toFixed(2)}€`;
             }
 
             else if (age.value === "under18") {
@@ -21,6 +27,10 @@ btn.addEventListener("click", function() {
                 console.log("cliente minorenne");
                 console.log(`Il prezzo è ${price.toFixed(2)}€`);
                 console.log(`Il prezzo col 20% di sconto è ${newPrice.toFixed(2)}€`);
+                document.querySelector("#toggle span>strong").innerHTML = `${name.value}`;
+                document.getElementById("train").innerHTML = Math.floor(Math.random() * 10) + 1;
+                document.getElementById("CPcode").innerHTML = Math.floor(Math.random() * (95000 - 90000)) + 95000;
+                document.getElementById("ticketPrice").innerHTML = `${newPrice.toFixed(2)}€`;
             }
 
             else if (age.value === "over65") {
@@ -30,6 +40,14 @@ btn.addEventListener("click", function() {
                 console.log("cliente over65");
                 console.log(`Il prezzo è ${price.toFixed(2)}€`);
                 console.log(`Il prezzo col 40% di sconto è ${newPrice.toFixed(2)}€`);
+                document.querySelector("#toggle span>strong").innerHTML = `${name.value}`;
+                document.getElementById("train").innerHTML = Math.floor(Math.random() * 10) + 1;
+                document.getElementById("CPcode").innerHTML = Math.floor(Math.random() * (95000 - 90000)) + 95000;
+                document.getElementById("ticketPrice").innerHTML = `${newPrice.toFixed(2)}€`;
             };
         }
+});
+
+document.getElementById("reset").addEventListener ("click", function() {
+    document.getElementById("toggle").classList.add("d-none");
 });
